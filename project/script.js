e3a789ccfd9db98d055eaf150100dcfd3dcf1a9d
FelixLangeder <f.langeder@students.htl-leonding.ac.at>
2026-04-07 20:12:30 +0200

    fixed landing page

---
+++ project/script.js
@@ -0,0 +1,10 @@
+function leaderboard() {
+    document.getElementById('nav').style.display = "none"
+    document.getElementById('leaderboard').style.display = "block"
+}
+
+function lp() {
+    document.getElementById('nav').style.display = "grid"
+    document.getElementById('leaderboard').style.display = "none"
+    document.getElementById('howTo').style.display = "none"
+}
