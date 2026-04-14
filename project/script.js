1d1ff9af6ac0998b9aa9160a9507dbf9127883e6
Felix Langeder <f.langeder@students.htl-leonding.ac.at>
2026-04-14 15:15:32 +0200

    loadingscreen gefixed

--- project/script.js
+++ project/script.js
@@ -1,41 +1,67 @@
 function leaderboard() {
     blackScreen()
     setTimeout(() => {
+        lightScreen()
         document.getElementById('lp').style.display = "none"
         document.getElementById('leaderboard').style.display = "block"
     }, 500)
 }
 
 function lp() {
-    document.getElementById('lp').style.display = "grid"
-    document.getElementById('leaderboard').style.display = "none"
-    document.getElementById('howTo').style.display = "none"
-    document.getElementById('levels').style.display = "none"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('lp').style.display = "grid"
+        document.getElementById('leaderboard').style.display = "none"
+        document.getElementById('howTo').style.display = "none"
+        document.getElementById('levels').style.display = "none"
+    }, 500)
 }
 
 function startGame() {
-    document.getElementById('lp').style.display = "none"
-    document.getElementById('levels').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('lp').style.display = "none"
+        document.getElementById('levels').style.display = "block"
+    }, 500)
 }
 
 function howTo() {
-    document.getElementById('lp').style.display = "none"
-    document.getElementById('howTo').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('lp').style.display = "none"
+        document.getElementById('howTo').style.display = "block"
+    }, 500)
 }
 
 function startLv1() {
-    document.getElementById('levels').style.display = "none"
-    document.getElementById('level1').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('levels').style.display = "none"
+        document.getElementById('level1').style.display = "block"
+    }, 500)
+
 }
 
 function startLv2() {
-    document.getElementById('levels').style.display = "none"
-    document.getElementById('level2').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('levels').style.display = "none"
+        document.getElementById('level2').style.display = "block"
+    }, 500)
 }
 
 function startLv3() {
-    document.getElementById('levels').style.display = "none"
-    document.getElementById('level3').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('levels').style.display = "none"
+        document.getElementById('level3').style.display = "block"
+    }, 500)
 }
 
 function loadCheckpoint() {
@@ -43,8 +69,12 @@ function loadCheckpoint() {
 }
 
 function goIn1() {
-    document.getElementById('level1').style.display = "none"
-    document.getElementById('rLevel1').style.display = "block"
+    blackScreen()
+    setTimeout(() => {
+        lightScreen()
+        document.getElementById('level1').style.display = "none"
+        document.getElementById('rLevel1').style.display = "block"
+    }, 500)
 }
 
 function goIn2() {
@@ -59,13 +89,13 @@ function blackScreen() {
     document.getElementById('blackBox').style.display = "block"
     document.getElementById('blackBox').classList.remove('fromBlack')
     document.getElementById('blackBox').classList.add('toBlack')
-    setTimeout(() => {
-        document.getElementById('blackBox').classList.remove('toBlack')
-        document.getElementById('blackBox').classList.add('fromBlack')
-        setTimeout(() => {
-            document.getElementById('blackBox').style.display = "none"
-        }, 500)
-    }, 500)
+    console.log('rage bait 2')
+}
+
+function lightScreen() {
+    document.getElementById('blackBox').classList.remove('toBlack')
+    document.getElementById('blackBox').classList.add('fromBlack')
+    document.getElementById('blackBox').style.display = "none"
 }
 
 setTimeout(() => {
